8fd9625c450667fd4d8ea4fed0a83edd91a28857
liam <380639608@qq.com>
2020-12-28 15:25:46 +0800

    hystrix web监控服务

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/lea/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.lea.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author lzc
+ * @create 2020-12-28 14:52
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
